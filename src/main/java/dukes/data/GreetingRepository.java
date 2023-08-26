937eeb66a37b70b3d234baa6644aace3e490575a
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-08-26 11:30:45 +0200

    Brush up samle code

--- src/main/java/dukes/data/GreetingRepository.java
+++ src/main/java/dukes/data/GreetingRepository.java
@@ -3,6 +3,10 @@ package dukes.data;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GreetingRepository extends CrudRepository<Greeting, Long> {
+
+    Optional<Greeting> findByNameIgnoreCase(String name);
 }
--- src/main/java/dukes/data/GreetingResource.java
+++ src/main/java/dukes/data/GreetingResource.java
@@ -6,25 +6,35 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/hello-world")
+import java.util.List;
+
+@Path("/greetings")
 public class GreetingResource {
 
     @Inject
     private GreetingRepository greetingRepository;
 
     @GET
-    @Produces("text/plain")
-    public String hello() {
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String findOne(@PathParam("name") String name) {
 
-       return greetingRepository.findAll()
-                .map(g -> g.getMessage())
-                .findFirst()
-                .orElse("Duke not found");
+       return greetingRepository.findByNameIgnoreCase(name)
+               .map(Greeting::getMessage)
+               .orElse(name + " not found");
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Greeting> findAll() {
+
+        return greetingRepository.findAll()
+                .toList();
     }
 
     @POST()
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addGreeting(@Valid Greeting greeting) {
+    public Response addGreeting(Greeting greeting) {
 
         Greeting saved = greetingRepository.save(greeting);
         return Response.ok("Created greeting: " + greeting.getId()).build();
