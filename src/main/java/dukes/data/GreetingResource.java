25d1a5afa053b488a0182decf30eebbefad2e8d5
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2025-03-19 13:07:11 -0700

    Java 24)

--- src/main/java/dukes/data/GreetingResource.java
+++ src/main/java/dukes/data/GreetingResource.java
@@ -1,7 +1,5 @@
 package dukes.data;
 
-import jakarta.inject.Inject;
-import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -11,32 +9,18 @@ import java.util.List;
 @Path("/greetings")
 public class GreetingResource {
 
-    @Inject
-    private GreetingRepository greetingRepository;
-
-    @GET
-    @Path("/{name}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String findOne(@PathParam("name") String name) {
-
-       return greetingRepository.findByNameIgnoreCase(name)
-               .map(Greeting::getMessage)
-               .orElse(name + " not found");
-    }
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Greeting> findAll() {
 
-        return greetingRepository.findAll()
-                .toList();
+        return List.of();
     }
 
     @POST()
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addGreeting(Greeting greeting) {
 
-        Greeting saved = greetingRepository.save(greeting);
-        return Response.ok("Created greeting: " + greeting.getId()).build();
+        return Response.ok().build();
     }
+
 }
